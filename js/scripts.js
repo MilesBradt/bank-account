a6f22260689572345d59f8d9453c05006fc7a9aa
Miles Bradt and Noah Schooler <noah@>
2018-10-30 10:11:22 -0700

    add deposit and withdraw functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,45 @@
-function BankInfo(name, email, balance) {
+function BankInfo(name, balance) {
   this.name = name,
-  this.email = email,
   this.balance = balance
+  console.log(this.balance);
+  // this.email = email,
 }
 
 function BankAccount() {
   this.bankInfo = [];
-  // this.currentId = 0;
+  this.currentId = 0;
 }
 
-BankAccountprototype.addInfo = function(info) {
+// function DepositAndWithdraw(deposit, withdraw) {
+//
+//   this.deposit = deposit,
+//   this.withdraw = withdraw
+//   console.log(deposit);
+//   console.log(withdraw);
+//   return (this.deposit, this.withdraw)
+//
+// }
+
+BankAccount.prototype.addInfo = function(info) {
+  BankInfo.id = this.assignId();
   this.bankInfo.push(info);
 }
 
+BankAccount.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
 
+BankAccount.prototype.depositing = function(deposit) {
+  var account = this.bankInfo[0]
+  return deposit + account.balance
+}
+
+BankAccount.prototype.withdrawing = function(withdraw) {
+  var account = this.bankInfo[0];
+  return account.balance - withdraw;
+}
+
+var newAccount = new BankAccount;
 $(document).ready(function() {
-
 });
